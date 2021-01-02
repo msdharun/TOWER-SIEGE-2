84204a1d0b063241c79b6c9d981642406ab78e84
msdharun <74610447+msdharun@users.noreply.github.com>
2021-01-02 11:19:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TOWER-SIEGE-2
+#**Rehan Project 30**
+
+Since you want code for projects, download this' zip, and run it locally! :D rehan!
---
+++ box.js
@@ -0,0 +1,37 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+     // isStatic:true,
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
+    }
+    this.x=x;
+    this.y=y;
+    this.visibility = 255;
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    World.add(world, this.body);
+  }
+  display(){
+    if(this.body.speed <7.5){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    strokeWeight(4);
+    stroke(0);
+  fill("turquoise");
+    rect(0, 0, this.width, this.height);
+    pop();}
+    else{
+world.remove(world,this.body);
+push();
+this.visibility = this.visibility - 5;
+pop();
+    }
+  }
+}
